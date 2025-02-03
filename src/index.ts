d7733fcc44a8505954f9f184f9c7f10c432c0638
BEIGNON Arthur <arthur.beignon1203@gmail.com>
2025-02-03 18:56:33 +0100

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { Request, Response } from 'express';
+
 
 export const app = express();
 const port = process.env.PORT || 3000;
@@ -10,3 +12,25 @@ export const server = app.listen(port);
 export function stopServer() {
   server.close();
 }
+
+//Liste de tous les pokémons
+app.get('/pokemons-cards', (_req: Request, _res: Response) => {
+  _res.status(200).send('Liste de tous les Pokémons');
+});
+
+
+app.get('/pokemons-cards/:pokemonCardId', (_req: Request, _res: Response) => {
+  _res.status(200).send('Liste du Pkémons');
+});
+
+app.post('/pokemon-cards', (_req: Request, _res: Response) => {
+  _res.status(200).send('Liste de tous les Pokémons');
+});
+
+app.patch('/pokemon-cards/:pokemonCardId', (_req: Request, _res: Response) => {
+  _res.status(200).send('Liste de tous les Pokémons');
+});
+
+app.delete('/pokemon-cards/:pokemonCardId:', (_req: Request, _res: Response) => {
+  _res.status(200).send('Liste de tous les Pokémons');
+});
