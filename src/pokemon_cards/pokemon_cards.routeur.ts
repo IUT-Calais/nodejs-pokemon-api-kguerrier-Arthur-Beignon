976cddce0b04af6393692609952fe6df8e8ebdb4
BEIGNON Arthur <arthur.beignon1203@gmail.com>
2025-03-05 15:16:23 +0100

    middleware en plus des routes

--- src/pokemon_cards/pokemon_cards.routeur.ts
+++ src/pokemon_cards/pokemon_cards.routeur.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { getPokemonCards, getPokemonCardID, postPokemonCards, patchPokemon, deletePokemonCard } from './pokemon_cards.controller';
+import { verifyJWT} from '../common/jwt.middleware'
+
 
 export const PokemonCardsRouteur = Router();
 
@@ -9,8 +11,8 @@ PokemonCardsRouteur.get('/', getPokemonCards);
 // Route pour obtenir un pokemon spécifique
 PokemonCardsRouteur.get('/:pokemon_id', getPokemonCardID);
 
-PokemonCardsRouteur.post('/', postPokemonCards);
+PokemonCardsRouteur.post('/',verifyJWT, postPokemonCards);
 
-PokemonCardsRouteur.patch('/:pokemon_id', patchPokemon)
+PokemonCardsRouteur.patch('/:pokemon_id', verifyJWT,  patchPokemon)
 
-PokemonCardsRouteur.delete('/:pokemon_id', deletePokemonCard);
+PokemonCardsRouteur.delete('/:pokemon_id',verifyJWT, deletePokemonCard);
